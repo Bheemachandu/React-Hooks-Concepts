61fe468651dd3a3485c0ca9a2332b44663eb8bdd
Bheemachandu <126513820+Bheemachandu@users.noreply.github.com>
2024-10-03 18:55:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import useContextHook from './useContextHook';
 import './App.css';
 
+
+import { useState, useEffect } from "react";
+
+
+const initialState = false
 function App() {
+  
+
+  const [toggleText, setToggleText] = useState(initialState)
+  console.log(toggleText)
+
+  const [count, setCount] = useState(0);
+  const [showText, setShowText] = useState(false);
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    if (count === 5) {
+      setShowText(true)
+    }
+    if (count===10){
+      fetchAllProducts()
+    }
+  }, [count])
+
+  async function fetchAllProducts() {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+      const result = await response.json();
+
+      if (result && result.products) setProductList(result.products)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>UseState Hook</h1>
+        <div>
+          {toggleText ? <p>Hello World</p> : null}
+
+          <button onClick={() => setToggleText(!toggleText)}>Toggle Text</button>
+        </div>
+      </div>
+      <div>
+        <h1>UseEffect Hook</h1>
+        <p>Count is {count}</p>
+        {showText ? <h3>Hello World</h3> : null}
+        <button onClick={() => setCount(count + 1)}>Increament Count</button>
+        <ul>
+          {productList && productList.length > 0 ? productList.map(item => <li>{item.title}</li>) : null}
+        </ul>
+      </div>
+      <useContextHook/>
     </div>
   );
 }
---
+++ src/useContextHook.js
@@ -0,0 +1,16 @@
+
+
+
+const useContextHook = () => {
+  return (
+    <div>
+      <h1>Use Context</h1>
+      <button>Change Theme</button>
+      <h1>Login Register</h1>
+      <button>Login</button>
+      <button>Register</button>
+    </div>
+  )
+}
+
+export default useContextHook
